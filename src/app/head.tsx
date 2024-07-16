4a12bff146e7d1ea35c885782082f4c11ab40564
João Victor <rocha.joao.victor.50@gmail.com>
2024-07-16 16:56:32 -0300

    teste de CEO

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,14 +8,23 @@ export default function CustomHead ({ title }: CustomHeadProps): JSX.Element {
   return (
     <>
       <title>{title}</title>
-      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <meta name="description" content="Donwload Videos" />
       <link
         rel="icon"
         href={icon.src}
         type="image/svg+xml"
         sizes="any"
       />
+      {/* Open Graph */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={'Baixe vídeos do TikTok sem marca d\'água com o Vid Drop. Rápido, fácil e gratuito!'} />
+      <meta property="og:url" content={'https://viddrop.netlify.app/tik-tok-video'} />
+      <meta property="og:image" content={'@/../public/favicon-32x32.png'} />
+      <meta property="og:type" content="website" />
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={'Baixe vídeos do TikTok sem marca d\'água com o Vid Drop. Rápido, fácil e gratuito!'} />
+      <meta name="twitter:image" content={'@/../public/favicon-32x32.png'} />
     </>
   )
 }
