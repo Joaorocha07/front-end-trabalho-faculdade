b1140a10c6b4ca61035d1e157b352012e09306f7
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2024-08-08 15:57:50 -0300

    Configurando a Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { type IRootLayout } from '@/types/global'
+import { Analytics } from '@vercel/analytics/react'
 
 import '../styles/globals.css'
 
@@ -8,7 +9,8 @@ export default function RootLayout ({ children }: IRootLayout): JSX.Element {
   return (
     <html lang="pt-BR">
       <body suppressHydrationWarning={true}>
-        <>{children}</>
+        {children}
+        <Analytics />
       </body>
     </html>
   )
