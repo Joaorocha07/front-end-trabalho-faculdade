ebdacb3e160a9f89e53cb5e810fe7a3c0e5a2a0c
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2024-08-12 15:09:45 -0300

    teste google

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,30 +7,30 @@ import Head from 'next/head'
 export default function CustomHead ({ title, description }: CustomHeadProps): JSX.Element {
   return (
     <>
-      <title>{title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content={description} />
-      <link
-        rel="icon"
-        href="/favicon.ico"
-        type="image/png"
-      />
-      {/* Open Graph */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={'https://viddrop.com.br/tik-tok-video'} />
-      <meta property="og:image" content="../../public/logo-branco.png" />
-      <meta property="og:type" content="website" />
-      {/* Twitter Card */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="../../public/logo-branco.png" />
       <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          type="image/png"
+        />
+        {/* Open Graph */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={'https://viddrop.com.br/tik-tok-video'} />
+        <meta property="og:image" content="../../public/logo-branco.png" />
+        <meta property="og:type" content="website" />
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="../../public/logo-branco.png" />
         {/* Google Analytics */}
         <script
           async
-          src={'https://www.googletagmanager.com/gtag/js?id=G-JNQBM7DN9Y'}
+          src="https://www.googletagmanager.com/gtag/js?id=G-JNQBM7DN9Y"
         />
         <script
           dangerouslySetInnerHTML={{
