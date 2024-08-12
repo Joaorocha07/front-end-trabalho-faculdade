e7a99ba14b7db912fa22b56a51722edba6ca6735
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2024-08-12 15:03:56 -0300

    google analistc

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { type CustomHeadProps } from '@/types/global'
 
+import Head from 'next/head'
+
 export default function CustomHead ({ title, description }: CustomHeadProps): JSX.Element {
   return (
     <>
@@ -11,19 +13,36 @@ export default function CustomHead ({ title, description }: CustomHeadProps): JS
       <link
         rel="icon"
         href="/favicon.ico"
-        type="image/x-icon"
+        type="image/png"
       />
       {/* Open Graph */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={'https://viddrop.com.br/tik-tok-video'} />
-      <meta property="og:image" content="/favicon.ico" />
+      <meta property="og:image" content="../../public/logo-branco.png" />
       <meta property="og:type" content="website" />
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="/favicon.ico" />
+      <meta name="twitter:image" content="../../public/logo-branco.png" />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={'https://www.googletagmanager.com/gtag/js?id=G-JNQBM7DN9Y'}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JNQBM7DN9Y');
+            `
+          }}
+        />
+      </Head>
     </>
   )
 }
