dd991464b80ce9533701436454826de3b059a5d3
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2024-08-12 14:36:13 -0300

    tomara de que certo - logo

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/no-string-refs */
 import React from 'react'
 
 import { type CustomHeadProps } from '@/types/global'
 
-import favicon32x32 from '../../public/logo-branco.png'
+// import favicon32x32 from '../../public/logo-branco.png'
 
 export default function CustomHead ({ title, description }: CustomHeadProps): JSX.Element {
   return (
@@ -12,20 +13,20 @@ export default function CustomHead ({ title, description }: CustomHeadProps): JS
       <meta name="description" content={description} />
       <link
         rel="icon"
-        href={favicon32x32.src}
-        type="image/x-icon"
+        ref="../../public/logo-branco.png"
+        type="image/png"
       />
       {/* Open Graph */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={'https://viddrop.com.br/tik-tok-video'} />
-      <meta property="og:image" content={favicon32x32.src} />
+      <meta property="og:image" content="../../public/logo-branco.png" />
       <meta property="og:type" content="website" />
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={favicon32x32.src} />
+      <meta name="twitter:image" content="../../public/logo-branco.png" />
     </>
   )
 }
