8355eeee19ebc8194bd0ace6f99e6bbc11b98277
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2024-08-12 15:49:29 -0300

    ajuste

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { type CustomHeadProps } from '@/types/global'
 
 import Script from 'next/script'
+import Head from 'next/head'
 
 export default function CustomHead ({ title, description }: CustomHeadProps): JSX.Element {
   return (
     <>
-      <head>
+      <Head>
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
@@ -27,23 +28,23 @@ export default function CustomHead ({ title, description }: CustomHeadProps): JS
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content="../../public/logo-branco.png" />
-        {/* Google Analytics */}
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-        />
+      </Head>
+      {/* Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
 
-        <Script id="ga-script" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-        </Script>
-      </head>
+      <Script id="ga-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+              `}
+      </Script>
     </>
   )
 }
